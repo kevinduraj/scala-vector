6ee9dc3c692b814bddc48ada978f13659d9e6ca9
Kevin Duraj <kevin.duraj@mdinsider.com>
2015-08-03 16:48:21 -0700

    use java stream

--- src/main/scala/Hello.scala
+++ src/main/scala/Hello.scala
@@ -1,12 +1,16 @@
 import scala.io.Source
+import java.io.InputStream
 
 object Hello {
 
   def main(args: Array[String]): Unit = {
 
     println("Loading Data to Vector")
+    //val vector: Vector[String] = Source.fromFile("/resources/test.txt").getLines.toVector
 
-    val vector: Vector[String] = Source.fromFile("/Users/kduraj/github/scala-vector/src/main/resources/test.txt").getLines.toVector
+    val stream : InputStream = getClass.getResourceAsStream("/test.txt")
+    val vector = scala.io.Source.fromInputStream( stream ).getLines.toVector
+
     println(vector.mkString("|"))
 
   }
