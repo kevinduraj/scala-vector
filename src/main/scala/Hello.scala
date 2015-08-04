f4f278aaaadf8715efae926d4442b24a591f0493
Kevin Duraj <kevin.duraj@mdinsider.com>
2015-08-03 17:17:36 -0700

    scala-vector-parallel

--- src/main/scala/Hello.scala
+++ src/main/scala/Hello.scala
@@ -1,25 +1,31 @@
 import scala.io.Source
 import java.io.InputStream
+import scala.collection.parallel.immutable.ParVector
 
 object Hello {
 
   def main(args: Array[String]): Unit = {
 
-    fromFile
+    parallelFromFile
 
   }
 
-  def fromFile: Unit = {
+  def parallelFromFile: Unit = {
     println("Loading Data from file to Vector")
 
-    val vector: Vector[String] = Source.fromFile("src/main/resource/test.txt").getLines.toVector
+    val vector: ParVector[String] = Source.fromFile("src/main/resource/test.txt").getLines.toVector.par
+
     println(vector.mkString("|"))
   }
 
-  def stream: Unit = {
+  def parallelFromStream: Unit = {
+
     println("Loading Data from stream to Vector")
+
     val stream: InputStream = getClass.getResourceAsStream("/src/main/resource/test.txt")
-    val vector = scala.io.Source.fromInputStream(stream).getLines.toVector
+    val vector: ParVector[String] = scala.io.Source.fromInputStream(stream).getLines.toVector.par
+
     println(vector.mkString("|"))
   }
+
 }
