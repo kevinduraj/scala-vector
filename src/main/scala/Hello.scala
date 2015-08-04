b20fecc50c9ea3621476379a6b2f98f5653e6af5
Kevin Duraj <kevin.duraj@mdinsider.com>
2015-08-03 17:06:35 -0700

    from file

--- src/main/scala/Hello.scala
+++ src/main/scala/Hello.scala
@@ -5,14 +5,21 @@ object Hello {
 
   def main(args: Array[String]): Unit = {
 
-    println("Loading Data to Vector")
-    //val vector: Vector[String] = Source.fromFile("/resources/test.txt").getLines.toVector
-
-    val stream : InputStream = getClass.getResourceAsStream("/test.txt")
-    val vector = scala.io.Source.fromInputStream( stream ).getLines.toVector
-
-    println(vector.mkString("|"))
+    fromFile
 
   }
 
+  def fromFile: Unit = {
+    println("Loading Data from file to Vector")
+
+    val vector: Vector[String] = Source.fromFile("src/main/resource/test.txt").getLines.toVector
+    println(vector.mkString("|"))
+  }
+
+  def stream: Unit = {
+    println("Loading Data from stream to Vector")
+    val stream: InputStream = getClass.getResourceAsStream("/src/main/resource/test.txt")
+    val vector = scala.io.Source.fromInputStream(stream).getLines.toVector
+    println(vector.mkString("|"))
+  }
 }
